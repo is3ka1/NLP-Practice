c1369ed41b7274351b4cc3e3525ff8d457cddcac
isekai <brian1689878638@gmail.com>
2019-07-10 13:47:27 +0800

    Add 2+3-gram

--- week1/chinese_news_analyse.py
+++ week1/chinese_news_analyse.py
@@ -9,22 +9,22 @@ from article_loader import ArticleLoader
 article_loader =  ArticleLoader('chinese_zh-TW_corpora.yaml')
 articles = article_loader.load()
 
-articles = [re.sub('[^\u4E00-\u9FA5A-Za-z0-9]', ' ', article) for article in articles]
+articles = [re.sub('[^\u4E00-\u9FA5A-Za-z0-9]', ' ', article)
+    for article in articles]
 
 jieba.set_dictionary('dict.txt.big')
 jieba.load_userdict('dict.txt')
 
-def tokenizer(sentence, flit=' ', **kwargs):
-    for token in jieba.cut(sentence, **kwargs):
-        if not token == flit:
-            yield token
+def tokenizer(sentence, flit=[' '], **kwargs):
+    return [token for token in jieba.cut(sentence, **kwargs)
+        if token not in flit]
 
-tfid_vectorizer = TfidfVectorizer(tokenizer=tokenizer)
-matrix = tfid_vectorizer.fit_transform(articles)
+def similarity(articles, **kwargs):
+    tfid_vectorizer = TfidfVectorizer(tokenizer=tokenizer, **kwargs)
+    matrix = tfid_vectorizer.fit_transform(articles)
+    return cosine_similarity(matrix)[0, 1]
 
-print(f'Similarity: {cosine_similarity(matrix)[0, 1]}')
 
-
-# tokens_space = set.union(*[set(tokenizer(article)) for article in articles])
-# print(matrix.A.shape[1] == len(tokens_space))                     # True
-# print(set(tfid_vectorizer.get_feature_names()) == tokens_space)   # True
+print(f'1-gram TF-IDF cosine similarity: {similarity(articles)}')
+print(f'2-gram + 3-gram TF-IDF cosine similarity: \
+    {similarity(articles, ngram_range=(2, 3))}')
--- week1/english_news_analyse.py
+++ week1/english_news_analyse.py
@@ -6,7 +6,13 @@ from article_loader import ArticleLoader
 article_loader =  ArticleLoader('english_corpora.yaml')
 articles = article_loader.load()
 
-tfid_vectorizer = TfidfVectorizer(ngram_range=(1, 2))
-matrix = tfid_vectorizer.fit_transform(articles)
+def similarity(articles, **kwargs):
+    tfid_vectorizer = TfidfVectorizer(**kwargs)
+    matrix = tfid_vectorizer.fit_transform(articles)
+    return cosine_similarity(matrix)[0, 1]
 
-print(f'Similarity: {cosine_similarity(matrix)[0, 1]}')
+
+print(f'1-gram TF-IDF cosine similarity: {similarity(articles)}')
+
+print(f'2-gram + 3-gram TF-IDF cosine similarity: \
+    {similarity(articles, ngram_range=(2, 3))}')
