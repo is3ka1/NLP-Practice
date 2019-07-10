b48a6a8d766deba1fa67efba752d2dce332d740f
isekai <brian1689878638@gmail.com>
2019-07-10 13:48:49 +0800

    Add pure jieba version

---
+++ week1/chinese_news_analyse_pure_jieba.py
@@ -0,0 +1,34 @@
+import jieba
+import jieba.analyse
+
+from math import sqrt
+
+from article_loader import ArticleLoader
+
+article_loader =  ArticleLoader('chinese_zh-TW_corpora.yaml')
+articles = article_loader.load()
+
+jieba.set_dictionary('dict.txt.big')
+jieba.load_userdict('dict.txt')
+
+term_with_tfidfs = [jieba.analyse.tfidf(article, topK=False, withWeight=True)
+    for article in articles]
+
+term_2_tfidfs = [dict(twt) for twt in term_with_tfidfs]
+
+features = list(
+    set.union(*[set(list(zip(*twt))[0]) for twt in term_with_tfidfs]))
+
+matrix = [[term_2_tfidf.get(term, 0.) for term in features]
+    for term_2_tfidf in term_2_tfidfs]
+
+def distance(vector):
+    return sqrt(sum(c ** 2 for c in vector))
+
+def dot(vectors):
+    return sum(a * b for a, b in zip(*vectors))
+
+def cosine_similarity(vectors):
+    return dot(vectors) / (distance(vectors[0]) * distance(vectors[1]))
+
+print(f'1-gram TF-IDF cosine similarity: {cosine_similarity(matrix)}')
