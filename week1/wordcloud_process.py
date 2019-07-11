cd77cc5574d894371d44a260dcccf1c1230193ee
isekai <brian1689878638@gmail.com>
2019-07-12 05:50:59 +0800

    Third line too long, break it

--- week1/wordcloud_process.py
+++ week1/wordcloud_process.py
@@ -1,6 +1,7 @@
 from wordcloud import WordCloud
 
-wc = WordCloud(font_path='NotoSansCJKtc-Regular.otf', width=1024, height=720, background_color='white')
+wc = WordCloud(font_path='NotoSansCJKtc-Regular.otf', width=1024, height=720,
+               background_color='white')
 
 def wordcloud_img(freqs: dict, path):
     wc.generate_from_frequencies(freqs)
