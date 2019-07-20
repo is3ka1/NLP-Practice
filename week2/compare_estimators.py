cf7b96df3b07795e6ca17deb4bc0e735624633ab
isekai <brian1689878638@gmail.com>
2019-07-20 18:27:12 +0800

    compare with svm navie_bayes

--- week2/compare_estimators.py
+++ week2/compare_estimators.py
@@ -3,24 +3,27 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegressionCV
 from sklearn.naive_bayes import BernoulliNB
-from sklearn.pipeline import make_pipeline
+from sklearn.svm import SVC
+
+from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 df = pd.read_csv('SPAM text message 20170820 - Data.csv')
 
-clf = make_pipeline(
-    TfidfVectorizer(stop_words='english'),
-    LogisticRegressionCV(cv=5 , solver='newton-cg', scoring='roc_auc'))
+clf = Pipeline([('tfidf', TfidfVectorizer(stop_words='english')),
+                ('clf', None)])
 
 parameters = {
-    'tfidfvectorizer__ngram_range': [(1, 1), (1, 2), (1, 3)],
-    'logisticregressioncv__cv': [3, 4, 5],
+    'tfidf__ngram_range': [(1, 1), (1, 2), (1, 3)],
+    'clf': [LogisticRegressionCV(cv=5 , solver='newton-cg', scoring='roc_auc'),
+            SVC(gamma='auto'), BernoulliNB()],
 }
 
 clf = GridSearchCV(clf, parameters, cv=4, scoring='roc_auc')
@@ -32,18 +35,16 @@ Y = pd.get_dummies(df['Category'])['spam']
 x_train, x_test, y_train, y_test = train_test_split(X, Y)
 
 clf.fit(x_train, y_train)
+print(clf.best_params_)
+print(f'score: {clf.score(x_test, y_test)}')
 
 
 y_pred = clf.predict(x_test)
 cm = confusion_matrix(y_test, y_pred)
 sns.heatmap(cm, annot=True, fmt='d')
-
-print(f'score: {clf.score(x_test, y_test)}')
+plt.show()
 
 samples = df.loc[y_test.index].copy()
 samples['true_result'] = y_test
 samples['model_perdict'] = y_pred
 print(samples.sample(n=10))
-
-
-print(clf.best_params_)
